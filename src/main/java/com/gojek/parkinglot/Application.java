39d58c45eee205106723303843739b5dd4112155
gaurav sharma <gaurav.ss@PW-C02VN0E2G8WN.local>
2020-06-22 09:43:16 +0530

    first commit

---
+++ src/main/java/com/gojek/parkinglot/Application.java
@@ -0,0 +1,12 @@
+package com.gojek.parkinglot;
+
+/**
+ * @author : gaurav.ss
+ * @since : 22/06/20
+ **/
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Staring Gojek Assignment: 09:36am");
+
+    }
+}
